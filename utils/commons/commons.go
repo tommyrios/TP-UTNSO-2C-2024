5168939873aa9defda30baf1e330644a5b60bf98
TotiGonzalez <tomgonzalez@frba.utn.edu.ar>
2024-09-16 17:03:35 -0300

    Commit CPU muy muy inicial

--- utils/commons/commons.go
+++ utils/commons/commons.go
@@ -49,3 +49,15 @@ func CodificarJSON(w io.Writer, responseStruct interface{}) error {
 	}
 	return err
 }
+
+type Registros struct {
+	PC uint32 `json:"pc"`
+	AX uint32 `json:"ax"`
+	BX uint32 `json:"bx"`
+	CX uint32 `json:"cx"`
+	DX uint32 `json:"dx"`
+	EX uint32 `json:"ex"`
+	FX uint32 `json:"fx"`
+	GX uint32 `json:"gx"`
+	HX uint32 `json:"hx"`
+}
