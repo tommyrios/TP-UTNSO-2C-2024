efa02e86b850b3e530071ed0f4a40de6bcd3de70
Tomás <tomgonzalez@frba.utn.edu.ar>
2024-11-30 17:52:44 -0300

    File System 100%

--- filesystem/handlers/handlers.go
+++ filesystem/handlers/handlers.go
@@ -4,12 +4,16 @@ package handlers
 //la petición de creación de un nuevo archivo de DUMP.
 //En la petición deberá venir el nombre del archivo, el tamaño y el contenido a grabar en el mismo
 import (
+	"bytes"
+	"encoding/binary"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/sisoputnfrba/tp-golang/filesystem/globals"
+	"github.com/sisoputnfrba/tp-golang/filesystem/inicializacion"
 	"github.com/sisoputnfrba/tp-golang/utils/commons"
 )
 
@@ -22,6 +26,7 @@ type RequestDumpMemory struct {
 
 func CrearArchivoDump(w http.ResponseWriter, r *http.Request) {
 	var archivo RequestDumpMemory
+	var nombreArchivo string
 	//var puntero uint32
 	err := commons.DecodificarJSON(r.Body, &archivo)
 
@@ -36,12 +41,41 @@ func CrearArchivoDump(w http.ResponseWriter, r *http.Request) {
 
 	if !espacioDisponible {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Error al realizar el dump de memoria"))
+		w.Write([]byte("Error al realizar el dump de memoria: No hay espacio disponible"))
 		log.Printf("Error al realizar el dump de memoria - (PID:TID) - (%d:%d)", archivo.Pid, archivo.Tid)
 	}
 
 	//Paso2: Reservar espacio en el FS para el archivo
-	bloquesReservados, err := ReservarBloques(bloquesNecesarios)
+	bloqueIndice, bloquesReservados, err := ReservarBloques(bloquesNecesarios)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error al realizar el dump de memoria: No se pudieron reservar los bloques necesarios"))
+		log.Printf("Error al realizar el dump de memoria - (PID:TID) - (%d:%d)", archivo.Pid, archivo.Tid)
+	}
+
+	//Paso3: Crear el archivo de metadata
+	ruta := filepath.Join(globals.FSConfig.MountDir, "/files")
+	err, nombreArchivo = inicializacion.CrearMetadata(ruta, archivo.Pid, archivo.Tid, archivo.Tamanio, bloqueIndice)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error al realizar el dump de memoria: No se pudo crear el archivo de metadata"))
+		log.Printf("Error al realizar el dump de memoria - (PID:TID) - (%d:%d)", archivo.Pid, archivo.Tid)
+	}
+
+	log.Printf("## Bloque asignado: %d - Archivo: %s - Bloques Libres: %d", bloqueIndice, nombreArchivo, cuantosLibresHay())
+	log.Printf("## Archivo Creado: %s - Tamaño: %d", nombreArchivo, archivo.Tamanio)
+
+	//Paso4: Escribir el contenido en los bloques reservados
+	err = EscribirContenido(bloqueIndice, bloquesReservados, nombreArchivo, archivo.Contenido)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error al realizar el dump de memoria: No se pudo escribir el contenido en los bloques reservados"))
+		log.Printf("Error al realizar el dump de memoria - (PID:TID) - (%d:%d)", archivo.Pid, archivo.Tid)
+	}
+
+	log.Printf("## Fin de solicitud - Archivo: %s", nombreArchivo)
 
 }
 
@@ -50,6 +84,78 @@ func VerificarEspacioDisponible(tamanio int) (bool, int) {
 	bloquesNecesarios := (tamanio + globals.FSConfig.BlockSize - 1) / globals.FSConfig.BlockSize
 	bloquesLibres := cuantosLibresHay()
 	return bloquesLibres >= bloquesNecesarios, bloquesNecesarios
+}
+
+func ReservarBloques(tamanio int) (int, []int, error) {
+	var bloquesDatos []int
+
+	rutaBit := filepath.Join(globals.FSConfig.MountDir, "bitmap.dat")
+	bitmap := LeerBitmap()
+
+	var bloqueIndice int = -1
+
+	for i := 0; i < len(bitmap)*8; i++ {
+		byteIndex := i / 8
+		bitIndex := i % 8
+
+		if (bitmap[byteIndex] & (1 << bitIndex)) == 0 {
+			if bloqueIndice == -1 {
+				bloqueIndice = i // Reservar el primer bloque como índice
+				bitmap[byteIndex] |= (1 << bitIndex)
+			} else {
+				bloquesDatos = append(bloquesDatos, i)
+				bitmap[byteIndex] |= (1 << bitIndex)
+			}
+		}
+
+		if len(bloquesDatos) == tamanio {
+			break
+		}
+	}
+
+	err := ActualizarBitmap(rutaBit, bitmap)
+	if err != nil {
+		return -1, nil, err
+	}
+
+	return bloqueIndice, bloquesDatos, nil
+}
+
+func EscribirContenido(bloqueIndice int, bloquesReservados []int, nombreArchivo string, contenido string) error {
+	ruta := filepath.Join(globals.FSConfig.MountDir, "bloques.dat")
+	archivo, err := os.OpenFile(ruta, os.O_WRONLY, 0644)
+	if err != nil {
+	}
+
+	defer archivo.Close()
+
+	//PUNTERO
+	puntero := SliceToBytes(bloquesReservados)
+	offsetIndice := bloqueIndice * globals.FSConfig.BlockSize
+	log.Printf("## Acceso Bloque - Archivo: %s - Tipo Bloque: INDICE - Bloque File System %d", nombreArchivo, bloqueIndice)
+	_, err = archivo.WriteAt(puntero, int64(offsetIndice))
+	if err != nil {
+		return err
+	}
+
+	//BLOQUES
+	partes := DividirEnBloques([]byte(contenido), globals.FSConfig.BlockSize)
+	for i, bloque := range bloquesReservados {
+		if i >= len(partes) {
+			break
+		}
+		offsetBloque := bloque * globals.FSConfig.BlockSize
+		log.Printf("## Acceso Bloque - Archivo: %s - Tipo Bloque: DATOS - Bloque File System %d", nombreArchivo, bloque)
+		_, err := archivo.WriteAt(partes[i], int64(offsetBloque))
+		if err != nil {
+			return err
+		}
+
+		// Simular retardo
+		time.Sleep(100 * time.Millisecond) // Ajusta el tiempo según sea necesario
+	}
+
+	return nil
 }
 
 func cuantosLibresHay() int {
@@ -89,32 +195,20 @@ func ActualizarBitmap(ruta string, bitmap []byte) error {
 	return nil
 }
 
-func ReservarBloques(tamanio int) ([]int, error) {
-	var bloquesReservados []int
-
-	rutaBit := filepath.Join(globals.FSConfig.MountDir, "bitmap.dat")
-	bitmap := LeerBitmap()
-
-	for i := 0; i < len(bitmap)*8; i++ {
-		byteIndex := i / 8
-		bitIndex := i % 8
-
-		if (bitmap[byteIndex] & (1 << bitIndex)) == 0 {
-			bloquesReservados = append(bloquesReservados, i)
-			// Marcar el bloque como ocupado
-			bitmap[byteIndex] |= (1 << bitIndex)
-		}
-
-		if len(bloquesReservados) == tamanio {
-			break
-		}
+func DividirEnBloques(contenido []byte, size int) [][]byte {
+	var bloques [][]byte
+	for size < len(contenido) {
+		bloques = append(bloques, contenido[:size])
+		contenido = contenido[size:]
 	}
+	bloques = append(bloques, contenido) // Agregar el resto
+	return bloques
+}
 
-	err := ActualizarBitmap(rutaBit, bitmap)
-	if err != nil {
-		return nil, err
+func SliceToBytes(ints []int) []byte {
+	buf := new(bytes.Buffer)
+	for _, n := range ints {
+		binary.Write(buf, binary.LittleEndian, int32(n)) // Convierte cada entero a un int32 en formato LittleEndian
 	}
-
-	return bloquesReservados, nil
-
+	return buf.Bytes()
 }
--- filesystem/inicializacion/inicializacion.go
+++ filesystem/inicializacion/inicializacion.go
@@ -75,25 +75,23 @@ func crearBloques(ruta string) error {
 	return nil
 }
 
-func crearMetadata(ruta string, pid int, tid int, tamaño int, indexBlock int) error {
-	timestamp := time.Now().Format("15:04:05.000")
+func CrearMetadata(ruta string, pid int, tid int, tamaño int, indexBlock int) (error, string) {
+	timestamp := time.Now().Format("150405.000")
 	nombreArchivo := fmt.Sprintf("%d-%d-%s.dmp", pid, tid, timestamp)
 
-	files := filepath.Join(ruta, nombreArchivo)
-	err := os.MkdirAll(files, os.ModePerm)
+	rutaArchivo := filepath.Join(ruta, nombreArchivo)
+
+	err := os.MkdirAll(ruta, os.ModePerm)
 	if err != nil {
-		return fmt.Errorf("error creando carpeta /files: %w", err)
+		return fmt.Errorf("error creando directorio %s: %w", ruta, err), ""
 	}
 
-	rutaMetadata := filepath.Join(files, nombreArchivo)
-
-	archivo, err := os.Create(rutaMetadata)
+	archivo, err := os.Create(rutaArchivo)
 	if err != nil {
-		return fmt.Errorf("error creando archivo de metadata: %w", err)
+		return fmt.Errorf("error creando archivo de metadata %s: %w", rutaArchivo, err), ""
 	}
 	defer archivo.Close()
 
-	// Crear la estructura Metadata
 	metadata := globals.Metadata{
 		IndexBlock: indexBlock,
 		Size:       tamaño,
@@ -101,13 +99,13 @@ func crearMetadata(ruta string, pid int, tid int, tamaño int, indexBlock int) e
 
 	json, err := commons.CodificarJSON(metadata)
 	if err != nil {
-		return fmt.Errorf("error codificando metadata en JSON: %w", err)
+		return fmt.Errorf("error codificando metadata a JSON: %w", err), ""
 	}
 
 	_, err = archivo.Write(json)
 	if err != nil {
-		return fmt.Errorf("error escribiendo metadata en archivo: %w", err)
+		return fmt.Errorf("error escribiendo metadata en el archivo: %w", err), ""
 	}
 
-	return nil
+	return nil, nombreArchivo
 }
